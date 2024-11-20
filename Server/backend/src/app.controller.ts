cbea1684d2aae06e7a0677bdc864021c1a459ef3
Mohamed Darify <mdarify@e2r9p1.1337.ma>
2024-11-20 08:49:18 +0100

    react-pro

--- Server/backend/src/app.controller.ts
+++ Server/backend/src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Controller, Post, Body, Get, Param, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
-@Controller()
+@Controller('gateway')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTH_SERVICE') private readonly authService: ClientProxy,
+    @Inject('PROFILE_SERVICE') private readonly profileService: ClientProxy,
+  ) {}
   @Get()
   getHello(): string {
     return this.appService.getHello();
